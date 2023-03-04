05f3d06b4deb3bad05980a04b68dbe5dc56cd014
Shab20 <122863503+Shab20@users.noreply.github.com>
2023-03-04 23:22:41 +0400

    Final

--- show_gif.py
+++ show_gif.py
@@ -1,45 +1,26 @@
 from tkinter import *
-from PIL import Image, ImageTk
+import time
+import os
 
 root = Tk()
-file = 'gif.gif'
+frameCnt = 12
 
-info = Image.open(file)
-frames = info.n_frames
-print(frames)
+# check git path
+frames = [PhotoImage(file='gif.gif',format='gif -index %i' % i) for i in range(frameCnt)]
 
-im = [ImageTk.PhotoImage(file=file,format=f'gif -index{i}') for i in range(frames)]
+def update(ind):
+    frame = frames[ind]
+    ind += 1
+    if ind == frameCnt:
+        ind = 0
+    label.configure(image=frame)
+    root.after(100, update, ind)
 
-gif_label = Label(image=im[0])
-gif_label.pack()
-
-anim = None
-count = 0
-delay = 50
-
-def animation():
-    global count, anim
-    count = (count + 1) % frames
-    gif_label.configure(image=im[count])
-    anim = root.after(delay, animation)
-
-def start_animation():
-    global anim
-    anim = root.after(delay, animation)
-
-def stop_animation():
-    global anim
-    if anim:
-        root.after_cancel(anim)
-        anim = None
-
-start_button = Button(text="Start", command=start_animation)
-start_button.pack()
-
-stop_button = Button(text="Stop", command=stop_animation)
-stop_button.pack()
-
+label = Label(root)
+label.pack()
+root.after(0, update, 0)
 root.mainloop()
+
 
 
 
